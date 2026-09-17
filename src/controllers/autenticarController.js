951480f958c3d4fd071ff56ca5112b2f0beaa559
SENA <nico.luque2806@gmail.com>
2026-09-17 16:14:35 -0500

    Cambios 17-09 v3

---
+++ src/controllers/autenticarController.js
@@ -0,0 +1,8 @@
+//creamos las funciones para utilizar en el router
+const iniciarSesion = async (req,res)=>{
+    try {
+
+    }   catch (error) {
+        
+    }
+}
--- src/routes/autenticarRouter.js
+++ src/routes/autenticarRouter.js
@@ -2,8 +2,15 @@ const {Router} = require('express')
 
 const enrutador = Router()
 // esta funcion la vamos a pasar al controlador
+// importamos del controlador
+const iniciarSesion = require("../controllers/autenticarController.js")
 enrutador.get("/", (req,res) => {
     res.json({mensaje: "ruta de autenticacion"})
 })
 
+enrutador.post("/login", iniciarSesion)
+
+enrutador.post("/registro", (req,res)=> {
+    res.json({mensaje: "ruta de registro"})
+})
 module.exports = enrutador
